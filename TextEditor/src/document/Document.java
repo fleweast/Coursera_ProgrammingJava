2dddc9fda49443397927e3d65a616e4fe6b72919
Elena <igorevna737@gmail.com>
2021-03-28 12:44:34 +0300

    Realize getFleschScore() and related methods

---
+++ TextEditor/src/document/Document.java
@@ -0,0 +1,134 @@
+package document;
+
+/** 
+ * A class that represents a text document
+ * @author UC San Diego Intermediate Programming MOOC team
+ */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class Document {
+
+	private String text;
+
+	protected Document(String text)
+	{
+		this.text = text;
+	}
+
+	protected List<String> getTokens(String pattern)
+	{
+		ArrayList<String> tokens = new ArrayList<String>();
+		Pattern tokSplitter = Pattern.compile(pattern);
+		Matcher m = tokSplitter.matcher(text);
+		
+		while (m.find()) {
+			tokens.add(m.group());
+		}
+		
+		return tokens;
+	}
+	
+	/** This is a helper function that returns the number of syllables
+	 * in a word.  You should write this and use it in your 
+	 * BasicDocument class.
+	 * 
+	 * You will probably NOT need to add a countWords or a countSentences 
+	 * method here.  The reason we put countSyllables here because we'll 
+	 * use it again next week when we implement the EfficientDocument class.
+	 * 
+	 * For reasons of efficiency you should not create Matcher or Pattern 
+	 * objects inside this method. Just use a loop to loop through the 
+	 * characters in the string and write your own logic for counting 
+	 * syllables.
+	 * 
+	 * @param word  The word to count the syllables in
+	 * @return The number of syllables in the given word, according to 
+	 * this rule: Each contiguous sequence of one or more vowels is a syllable, 
+	 *       with the following exception: a lone "e" at the end of a word 
+	 *       is not considered a syllable unless the word has no other syllables. 
+	 *       You should consider y a vowel.
+	 */
+	protected int countSyllables(String word)
+	{
+		// TODO: Implement this method so that you can call it from the 
+	    // getNumSyllables method in BasicDocument (module 2) and 
+	    // EfficientDocument (module 3).
+	    return 0;
+	}
+	
+	/** A method for testing
+	 * 
+	 * @param doc The Document object to test
+	 * @param syllables The expected number of syllables
+	 * @param words The expected number of words
+	 * @param sentences The expected number of sentences
+	 * @return true if the test case passed.  False otherwise.
+	 */
+	public static boolean testCase(Document doc, int syllables, int words, int sentences)
+	{
+		System.out.println("Testing text: ");
+		System.out.print(doc.getText() + "\n....");
+		boolean passed = true;
+		int syllFound = doc.getNumSyllables();
+		int wordsFound = doc.getNumWords();
+		int sentFound = doc.getNumSentences();
+		if (syllFound != syllables) {
+			System.out.println("\nIncorrect number of syllables.  Found " + syllFound 
+					+ ", expected " + syllables);
+			passed = false;
+		}
+		if (wordsFound != words) {
+			System.out.println("\nIncorrect number of words.  Found " + wordsFound 
+					+ ", expected " + words);
+			passed = false;
+		}
+		if (sentFound != sentences) {
+			System.out.println("\nIncorrect number of sentences.  Found " + sentFound 
+					+ ", expected " + sentences);
+			passed = false;
+		}
+		
+		if (passed) {
+			System.out.println("passed.\n");
+		}
+		else {
+			System.out.println("FAILED.\n");
+		}
+		return passed;
+	}
+	
+	
+	/** Return the number of words in this document */
+	public abstract int getNumWords();
+	
+	/** Return the number of sentences in this document */
+	public abstract int getNumSentences();
+	
+	/** Return the number of syllables in this document */
+	public abstract int getNumSyllables();
+	
+	/** Return the entire text of this document */
+	public String getText()
+	{
+		return this.text;
+	}
+	
+	/** return the Flesch readability score of this document */
+	public double getFleschScore()
+	{
+	/*	if (getNumSyllables() == 0 || getNumWords() == 0){
+			return  0;
+		}
+
+	 */
+		double numberSyllables = (double)getNumSyllables();
+		double numberWords = (double)getNumWords();
+		double numberSentences = (double)getNumSentences();
+		double res =  206.835 - 1.015 * (numberWords / numberSentences) -84.6 * (numberSyllables / numberWords);
+		return res;
+	}
+	
+}
